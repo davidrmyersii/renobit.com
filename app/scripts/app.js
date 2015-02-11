347bd8c31c0bb0d1de1f2319fe7d4796182cba8a
drmyersii <drmyersii@users.noreply.github.com>
2015-02-11 16:04:13 -0500

    First full project commit.
    Using Yeoman with Angular generator. Added grunt-recess for less
    compilation.

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name renobitApp
+ * @description
+ * # renobitApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('renobitApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name renobitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the renobitApp
+ */
+angular.module('renobitApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
