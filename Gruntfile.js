99f8fa99d241d5e08c0523412d2fd45467f1355b
drmyersii <drmyersii@users.noreply.github.com>
2015-02-18 15:58:58 -0500

    Fixed issue with font path reference in css.
    See Gruntfile.js under cssmin for options that fixed the problem. It is
    still an open issue on GitHub to get it fixed by default.

--- Gruntfile.js
+++ Gruntfile.js
@@ -267,26 +267,29 @@ module.exports = function (grunt) {
 		// By default, your `index.html`'s <!-- Usemin block --> will take care of
 		// minification. These next options are pre-configured if you do not wish
 		// to use the Usemin blocks.
-		// cssmin: {
-		//	 dist: {
-		//		 files: {
-		//			 '<%= yeoman.dist %>/styles/main.css': [
-		//				 '.tmp/styles/{,*/}*.css'
-		//			 ]
-		//		 }
-		//	 }
-		// },
+		cssmin: {
+			options: {
+				rebase: false
+			}
+			 // dist: {
+				//  files: {
+				// 	 '<%= yeoman.dist %>/styles/main.css': [
+				// 		 '.tmp/styles/{,*/}*.css'
+				// 	 ]
+				//  }
+			 // }
+		},
 		// uglify: {
-		//	 dist: {
-		//		 files: {
-		//			 '<%= yeoman.dist %>/scripts/scripts.js': [
-		//				 '<%= yeoman.dist %>/scripts/scripts.js'
-		//			 ]
-		//		 }
-		//	 }
+		// 	 dist: {
+		// 		 files: {
+		// 			 '<%= yeoman.dist %>/scripts/scripts.js': [
+		// 				 '<%= yeoman.dist %>/scripts/scripts.js'
+		// 			 ]
+		// 		 }
+		// 	 }
 		// },
 		// concat: {
-		//	 dist: {}
+		// 	 dist: {}
 		// },
 
 		imagemin: {
