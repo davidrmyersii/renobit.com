38e6d00f022ceb418b61b3e6faa18921eb140700
drmyersii <drmyersii@users.noreply.github.com>
2015-02-17 21:21:27 -0500

    Worked on more style and started on content
    Added fontello custom font stack. Removed unused controller and view.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,10 @@
  * # MainCtrl
  * Controller of the renobitApp
  */
-angular.module('renobitApp')
-	.controller('MainCtrl', function ($scope) {
-		$scope.awesomeThings = [
-			'HTML5 Boilerplate',
-			'AngularJS',
-			'Karma'
-		];
-	});
+angular.module('renobitApp').controller('MainCtrl', function ($scope) {
+	$scope.awesomeThings = [
+		'HTML5 Boilerplate',
+		'AngularJS',
+		'Karma'
+	];
+});
