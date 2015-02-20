da2150e571d82dd295322b6c412fa8bb25ee542e
drmyersii <drmyersii@users.noreply.github.com>
2015-02-20 17:09:31 -0500

    Build error fix.

--- app/scripts/directives/ng-util-onscroll.js
+++ app/scripts/directives/ng-util-onscroll.js
@@ -7,19 +7,19 @@
  * # utilOnscroll
  * Handles window on scroll events
  */
-angular.module('renobitApp')
-	.directive('utilOnscroll', function ($window)
-	{
-		return {
-			link: function (scope)
-			{
-				// angular.element($window).bind('scroll', function ()
-				// {
-				// 	var offset = this.pageYOffset;
-				// 	angular.element(document.getElementById('welcome')).css('top', (-1 * (offset/10)) + 'px');
+// angular.module('renobitApp')
+// 	.directive('utilOnscroll', function ($window)
+// 	{
+// 		return {
+// 			link: function (scope)
+// 			{
+// 				angular.element($window).bind('scroll', function ()
+// 				{
+// 					var offset = this.pageYOffset;
+// 					angular.element(document.getElementById('welcome')).css('top', (-1 * (offset/10)) + 'px');
 
-				// 	scope.$apply();
-				// });
-			}
-		};
-	});
+// 					scope.$apply();
+// 				});
+// 			}
+// 		};
+// 	});
